2b4e272537333d8438e558c29b32f4cfdb847e7c
Chase Moskal <chasemoskal@gmail.com>
2017-12-18 01:14:24 -0800

    clean up build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ rm -rf ./dist
 docker build -t webphero .
 
 # run the docker container, with 'dist' volume, and run the container script
-docker run --volume $PWD/dist:/dist --rm webphero /bin/bash -c "chmod -R 755 /work/scripts && /work/scripts/container-script.sh"
+docker run --volume $PWD/dist:/dist --rm webphero /work/scripts/container-script.sh
 
 # set reasonable permissions to the 'dist' dir
 chmod -R 755 ./dist
--- scripts/container-script.sh
+++ scripts/container-script.sh
@@ -5,15 +5,15 @@
 ##
 
 cd /work/images \
-	&& cwebp dice.original.png -o dice.webp \
-	&& dwebp dice.webp -o dice.png
+  && cwebp dice.original.png -o dice.webp \
+  && dwebp dice.webp -o dice.png
 
 ##
 ## COPY DIST ARTIFACTS
 ##
 
-mkdir /dist/webpjs
-cp -r /work/libwebp/webp_js/*.{mem,js,html,webp} /dist/webpjs
-
 mkdir /dist/images
 cp /work/images/* /dist/images
+
+mkdir /dist/webpjs
+cp -r /work/libwebp/webp_js/*.{mem,js,html,webp} /dist/webpjs
