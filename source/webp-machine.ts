862bc451a39e5795be938073d80b935c30c368d8
Chase Moskal <chasemoskal@gmail.com>
2019-02-23 21:06:35 -0800

    tweak some typescript syntax

--- source/webp-machine.ts
+++ source/webp-machine.ts
@@ -49,9 +49,9 @@ export class WebpMachine {
 	 * Polyfill the webp format on the given <img> element
 	 */
 	async polyfillImage(image: HTMLImageElement): Promise<void> {
-		const webpSupport = await this.webpSupport
+		if (await this.webpSupport) return
 		const {src} = image
-		if (!webpSupport && /\.webp$/i.test(src)) {
+		if (/\.webp$/i.test(src)) {
 			if (this.cache[src]) {
 				image.src = this.cache[src]
 			}
@@ -73,8 +73,7 @@ export class WebpMachine {
 	async polyfillDocument({
 		document = window.document
 	}: PolyfillDocumentOptions = {}): Promise<void> {
-		const webpSupport = await this.webpSupport
-		if (webpSupport) return null
+		if (await this.webpSupport) return null
 		for (const image of Array.from(document.querySelectorAll("img"))) {
 			await this.polyfillImage(image)
 		}
