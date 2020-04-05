00bfc045008ee6a8e44f046c8a1e46729012f3de
theonetheycallneo <anthony@readythecatapult.com>
2020-04-05 09:07:31 -0700

    chore(tsc): lint

--- source/convert-binary-data.ts
+++ source/convert-binary-data.ts
@@ -3,13 +3,13 @@ const BASE64_MARKER = ";base64,";
 export const isBase64Url = (src: string) => src.indexOf(BASE64_MARKER) > -1;
 
 export const convertDataURIToBinary = (dataURI: string): Uint8Array => {
-  const base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-  const base64 = dataURI.substring(base64Index);
-  const raw = window.atob(base64);
-  const rawLength = raw.length;
-  const array = new Uint8Array(new ArrayBuffer(rawLength));
-  for (let i = 0; i < rawLength; i++) {
-    array[i] = raw.charCodeAt(i);
-  }
-  return array;
+    const base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+    const base64 = dataURI.substring(base64Index);
+    const raw = window.atob(base64);
+    const rawLength = raw.length;
+    const array = new Uint8Array(new ArrayBuffer(rawLength));
+    for (let i = 0; i < rawLength; i++) {
+        array[i] = raw.charCodeAt(i);
+    }
+    return array;
 };
